3b38c37781b5317621c26e654c9f7b7d290542b2
Marat Ibadinov <ibadinov@me.com>
2012-10-11 18:07:27 +0300

    configures on mac using apple-gnu-gnu combo, still does not detect MDNS

--- config/config.constant-string-class.m
+++ config/config.constant-string-class.m
@@ -12,11 +12,41 @@
    version 2 of the License, or (at your option) any later version.
 */
 
-/* must be compiled compile using -fconstant-string-class=NSConstantString
+/* must be compiled compile using -fconstant-string-class=FooConstantString
    as an option to gcc.  If it doesn't work, it means your gcc doesn't
    support this option. */
 
 #include "objc-common.g"
+
+#if defined (NeXT_RUNTIME) && defined (__OBJC2__)
+Class _MyConstantStringClassReference;
+
+@interface FooConstantString : Object
+{
+  char*       c_string;
+  unsigned    length;
+}
+@end
+
+@implementation FooConstantString
+
++ (void)load;
+{
+  /* Our string literals will have their "isa" pointer set to 
+   * _FooConstantStringClassReference, but it points nowhere.
+   * We must set it to compiler-generated class.
+   */
+  _MyConstantStringClassReference = objc_getClass("FooConstantString");
+}
+
+- (const char *)cString;
+{
+  return c_string;
+}
+
+@end
+
+#else /* new NeXT_RUNTIME */
 
 /* Define our custom constant string class */
 @interface FooConstantString
@@ -35,19 +65,20 @@ struct objc_class _FooConstantStringClassReference;
 #endif
 
 @implementation FooConstantString
-- (char *) customString
+- (char *) cString
 {
     return c_string;
 }
 @end
 
+#endif
 
 int main (int argc, char **argv)
 {
    /* Create a test constant string */
    FooConstantString *string = @"Antonio Valente";
 
-#ifdef NeXT_RUNTIME
+#if defined(NeXT_RUNTIME) && !defined (__OBJC2__)
    /* This memcpy is needed here due to a bug in ObjC gcc when using
       next runtime. It has to be done once per program and before
       the first message is sent to a constant string. Can't be moved to
@@ -59,16 +90,15 @@ int main (int argc, char **argv)
 #endif
 
    /* Check that it really works */
-   if (strcmp ([string customString], "Antonio Valente"))
+   if (strcmp ([string cString], "Antonio Valente"))
      {
        abort ();
      }
 
    /* Do another, more direct test. */
-   if (strcmp ([@"Jump" customString], "Jump"))
-       {
-         abort ();
-       }
+   if (strcmp ([@"Jump" cString], "Jump"))
+     {
+       abort ();
+     }
    return 0;
 }
-
